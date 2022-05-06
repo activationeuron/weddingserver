21a80755fdbca607fa73b56d2d90b52b28219c17
activationeuron <aforamitrai@gmail.com>
2022-05-06 20:47:47 +0530

    invitation with count

--- models/Invitations.js
+++ models/Invitations.js
@@ -4,9 +4,12 @@ const invitationsSchema = new mongoose.Schema({
   phone: { type: String, required: true, unique: true },
   limit: { type: String, required: true },
   name: { type: String, required: true },
-  events: {
-    type: Array,
-  },
+  events: [
+    {
+      name: String,
+      count: String,
+    },
+  ],
 });
 
 const Invitations = mongoose.model('Invitations', invitationsSchema);
